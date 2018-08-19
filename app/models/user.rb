f351789bcbaf65cd96925f25f913e2c026ee0cf2
i-higurashi <38269101+i-higurashi@users.noreply.github.com>
2018-08-19 17:12:52 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
- order2
+  
+  
         belongs_to :carts
         has_many :items
 
