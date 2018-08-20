ac986917da9f0a3c8cf39aa44140d1ccf6e73f92
taoka3331 <taokamasaharu@gmail.com>
2018-08-20 05:48:48 +0000

    cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,17 +2,18 @@ class Item < ApplicationRecord
 	has_many :discs
 	accepts_nested_attributes_for :discs, allow_destroy: true
 
-	has_many :line_items
-	before_destroy :referenced_by_line_item
+	has_many :cart_items
+	before_destroy :referenced_by_cart_item
 
 	private
-	def referenced_by_line_item
+	def referenced_by_cart_item
 
-		if line_items.empty?
+		if cart_items.empty?
 			return true
 		else
 			errors.add(:base, '品目が存在します。')
 			retuen false
 		end
 	end
+end
 
