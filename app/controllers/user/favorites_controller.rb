5c448382c38d13d091ceb6783f84c6119168b798
i-higurashi <sekainotobira.1009@gmail.com>
2018-08-27 09:26:06 +0000

    m

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,8 +1,12 @@
 class User::FavoritesController < ApplicationController
   layout 'user'
 	def index
-    @user = current_user
-    @favorite = Favorite.where(user_id: @user.id).all
+		if user_signed_in?
+	    @user = current_user
+	    @favorite = Favorite.where(user_id: @user.id).all
+	    else
+	    redirect_to new_user_session_path
+	    end
 	end
 
 
@@ -10,10 +14,14 @@ class User::FavoritesController < ApplicationController
 	def create
 	    @item_id = Item.find(params[:id]).id #特定のitemのid
 	    #item_idに@item_id、user_idに@user_idを入れて、Favoriteモデルに新しいオブジェクトを作る
+	    if user_signed_in?
 	    @favorite = Favorite.new(item_id: @item_id, user_id: current_user.id)
 	    @favorite.save
 	      #保存に成功した場合、一覧画面に戻る
 	    redirect_to user_favorites_path
+	    else
+	    redirect_to new_user_session_path
+	    end
     end
 
     #お気に入り削除用アクション
