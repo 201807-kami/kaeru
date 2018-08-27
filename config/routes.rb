decf007339248b2edd645f658d021bc1e06d0515
ayana0257 <ayana.1031.stkj@gmail.com>
2018-08-27 10:20:34 +0000

    create_purchase

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,8 @@ Rails.application.routes.draw do
 		resources :genres, only: [:index, :show]
 		resources :favorites, only: [:index, :destroy]
 		resources :carts, only: [:index, :new, :create, :show, :update, :destroy]
+		get '/order_items' => 'orders#order_items'
+
 		resources :orders, only: [:create, :new] do
 		get :complete
 		end
